953e8124c75c8b8ef78fa7b5fcd1746dfc19ced5
Bruno Moschetto <bruno.moschetto@gmail.com>
2024-07-05 16:41:17 +0200

    challenges from fridays workshop done :)

--- 03-String-Methods/01_string_wizard.rb
+++ 03-String-Methods/01_string_wizard.rb
@@ -2,19 +2,18 @@
 
 def transform_string(str)
   # TODO: 1: Remove leading and trailing whitespace
-
+  str.strip!
   # TODO: 2: Replace 'C' with 'D' and 'T' with 'Y'
-
+  str = str.sub("C", "D").sub("T", "Y")
   # TODO: 3: Convert the string to lowercase
-
+  str.downcase!
   # TODO: 4: Reverse the string
-
+  str.reverse!
   # TODO: 5: Insert a space after the 4th character
-
+  str.insert(4, " ")
   # TODO: 6: Capitalize the string
-
+  str.capitalize!
   # TODO: 7: Return the transformed string
-
 end
 
 # now run the code
--- 03-String-Methods/02_able.rb
+++ 03-String-Methods/02_able.rb
@@ -1,18 +1,19 @@
 def create_word(a_range)
   # TODO: Convert the range to an array
-
+  array = [*a_range]
   # TODO: Remove ['c', 'd'] from the original array by subtraction
-
+  array.delete('c')
+  array.delete('d')
   # TODO: Insert 'l' before the last element
-
+  array.insert(2, "l")
   # TODO: Join the elements to form a word
-
+  result = array.join()
   # TODO: Return the transformed object
-
+  return result
 end
 
 # Simple test for the create_word method
-passing_argument = nil # TODO: Replace 'nil' with a range of letters between a-e including e
+passing_argument = ["a", "b", "c", "d", "e"] # TODO: Replace 'nil' with a range of letters between a-e including e
 # now run the code
 
 # leave this cute test alone
